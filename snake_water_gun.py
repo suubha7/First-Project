6044f86e3a175a346859b8a3a6a3a92134d6c286
suubha7 <suubha090@gmail.com>
2025-06-26 23:39:27 +0530

    add  5 rounds

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -3,22 +3,35 @@ import random
 print("Welcome to Snake, Water , Gun Game!")
 options = ['snake', 'water', 'gun']
 
-computer = random.choice(options)
-user = input("Choose one Snake, Water, Gun : ").lower()
+user_score = 0
+computer_score = 0
+tia = 0
 
-if user not in options:
-    print("Invalid choice !")
+for i in range(1,6):
+    print(f"Round {i}")
 
-else:    
-    print("\nYou Chose:", user)
-    print("Computer Chose:",computer)
+    computer = random.choice(options)
+    user = input("Choose one Snake, Water, Gun : ").lower()
 
-    if computer == user:
-        print("It's a Tia!")
+    if user not in options:
+        print("Invalid choice!, Skip the round.\n")
+        continue
+    else:    
+        print("You Chose:", user)
+        print("Computer Chose:",computer)
 
-    elif (user == 'snake'and computer == 'water') or (user == 'water'and computer == 'gun') or (user == 'gun'and computer == 'snake'):
-        print("You Win!")
+        if computer == user:
+            print("It's a Tia!\n")
+            tia += 1
+        elif (user == 'snake'and computer == 'water') or (user == 'water'and computer == 'gun') or (user == 'gun'and computer == 'snake'):
+            print("You Win!\n")
+            user_score += 1
+        else:
+            print("You Lose!\n")
+            computer_score += 1
 
-    else:
-        print("You Lose!")
-
+print(f'Your Score : {user_score}\nComputer Score : {computer_score}\nTia : {tia}')
+if user_score > computer_score:
+    print("You win!")
+else:
+    print("Computer win!")
